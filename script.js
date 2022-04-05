d37b0bec54f3e780b8cc198e440e2650023d2a04
Amy Lightfoot <MrsAmyLightfoot@gmail.com>
2022-04-04 23:31:14 -0400

    updating files with format and beginning API

---
+++ script.js
@@ -0,0 +1,17 @@
+var APIKey = 'f6a50489796a30a62c225d62a793288a';
+var city = 
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+let weather = {
+    fetchWeather: function (city) {
+        fetch(("http://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&appid="
+        + this.APIKey)
+        )
+        .then((Response) => Response.json())
+        .then((Data) => console.log(Data));
+    }
+
+}
