e0de553ed28c2d133816ce365e4d0acba1e9fe0f
Amy Lightfoot <MrsAmyLightfoot@gmail.com>
2022-04-05 22:34:57 -0400

    additional JS attempts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var APIKey = 'f6a50489796a30a62c225d62a793288a';
-var city = 
+var city = document.getElementsByClassName("search-bar");
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
